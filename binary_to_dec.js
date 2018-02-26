37441122bf60f9a0aaf1b8e05f36dc758846ebf1
Elena Efimova <elenaefimova@MacBook-Air-Efimova.local>
2018-02-26 17:58:54 +0300

    firstTask

--- binary_to_dec.js
+++ binary_to_dec.js
@@ -3,18 +3,14 @@
 
 
 function binaryToDec(binaryNumber){
-    var a = [];
     var c = 0;
     for(var i = 0; i<binaryNumber.length; i++){
         if (binaryNumber[i]==0) {
-            a.push(0);
             c += Math.pow(2,i);
         }else if(binaryNumber[i]==1){
-            a.push(1);
             c += Math.pow(2,i);
         } else {
             console.log('Your number is undefined');
-            a.length = 0;
             break;
             }
         }
@@ -22,5 +18,5 @@ function binaryToDec(binaryNumber){
 
 }
 
-binaryToDec("fwedg");
+binaryToDec("101001");
 
