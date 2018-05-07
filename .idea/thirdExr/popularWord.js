115f159b425993271911712630bd092ff4245cf4
Elena Efimova <ees.elena@gmail.com>
2018-05-07 15:59:58 +0300

    242

--- .idea/thirdExr/popularWord.js
+++ .idea/thirdExr/popularWord.js
@@ -1,4 +1,4 @@
-//elena-efimova
+/*//elena-efimova
 //Вывести самое популярное слово
 function Word(word) {
     this.word = word;
@@ -61,5 +61,12 @@ function getWord(text) {
 }
 
 let res = getWord('Sed tempus ipsum quis eros tempus lacinia Cras finibus lorem ut lacinia egestas nunc nibh iaculis est convallis tincidunt mi mi sed nisl Sed porttitor aliquam elit ullamcorper tincidunt arcu euismod quis Mauris congue elit suscipit leo varius facilisis Cras et arcu sodales laoreet est vitae pharetra orci Integer eget nulla dictum aliquet justo semper molestie neque Maecenas bibendum lacus tincidunt auctor varius purus felis ullamcorper dui et laoreet ligula ex et risus Donec eget fringilla nibh Cras congue tincidunt accumsan Maecenas euismod eleifend elit ut rhoncus tortor sodales a Cras egestas finibus lorem non tempor tincidunt aera                                                               ');
-console.log(res);
+console.log(res);*/
 //getPopularWord('Sed tempus ipsum quis eros tempus lacinia Cras finibus lorem ut lacinia egestas nunc nibh iaculis est convallis tincidunt mi mi sed nisl Sed porttitor aliquam elit ullamcorper tincidunt arcu euismod quis Mauris congue elit suscipit leo varius facilisis Cras et arcu sodales laoreet est vitae pharetra orci Integer eget nulla dictum aliquet justo semper molestie neque Maecenas bibendum lacus tincidunt auctor varius purus felis ullamcorper dui et laoreet ligula ex et risus Donec eget fringilla nibh Cras congue tincidunt accumsan Maecenas euismod eleifend elit ut rhoncus tortor sodales a Cras egestas finibus lorem non tempor tincidunt aera                                                               ');
+
+
+
+new Promise((resolve, reject) => reject())
+.then(() => { throw new Error(2) })
+.catch(() => 1)
+.then((a) => console.log(a))
